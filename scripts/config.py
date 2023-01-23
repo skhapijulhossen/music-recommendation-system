67c406a63b13aefe7bdaf67b19015d8468347aff
skhapijulhossen <skhapijulhossen@gmail.com>
2023-01-23 20:27:13 +0530

    update searchETL

---
+++ scripts/config.py
@@ -0,0 +1,8 @@
+# Configurations
+access_token = "BQDENPSQFZSMBGSEXtQCY0xi7Aeap8NyVshA7hkVLjyvsJ2_4osqefbzcAmUHv-B1t86nVF6fBKfRsARNS-AH1V9nnr5qXIq-4baQnWBmRA1_7chzUSWMmEJl7DLtVRiPSdB7M8h82Ng8NToazucFIl6UHnxFArpFM3slZ_JBaJtN9vlo9rRk9F-zVpXl3kM9fU"
+artist_name_store = "../tempStore/artist_names.txt"
+artist_id_store = "../tempStore/artists_ids.txt"
+album_id_store = "../tempStore/album_ids.txt"
+track_json_store = "../rawDataStore/tracksDB.orc"
+tracks_data_store = "../rawDataStore/tracksDB.orc"
+track_feature_store = "../rawDataStore/tracksFeatureDB.orc"
